d29dc400f42820c9505f1e7fbec44c553849dc2b
Brandon Edens <brandon@cs.uri.edu>
2009-12-24 10:24:21 -0500

    Add initial html, url, and view to show a default page.

--- urls.py
+++ urls.py
@@ -1,4 +1,25 @@
+# Brandon Edens
+# AS220
+# 2009-12-24
+# Copyright (C) 2009 Brandon Edens <brandon@as220.org>
+#
+# This file is part of Jukebox.
+#
+# Jukebox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Jukebox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Jukebox.  If not, see <http://www.gnu.org/licenses/>.
+
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -8,10 +29,19 @@ urlpatterns = patterns('',
     # Example:
     # (r'^jukebox/', include('jukebox.foo.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     # (r'^admin/', include(admin.site.urls)),
+
+    (r'^$', 'jukebox.views.index'),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('django.views.static',
+    (r'^jukebox/static_media/(?P<path>.*)$', 'serve',
+     {'document_root': '/home/brandon/src/jukebox/static_media',
+      'show_indexes': True }),)
+
---
+++ views.py
@@ -0,0 +1,39 @@
+# Brandon Edens
+# 2009-12-24
+# Copyright (C) 2009 Brandon Edens <brandon@as220.org>
+#
+# This file is part of Jukebox.
+#
+# Jukebox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Jukebox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Jukebox. If not, see <http://www.gnu.org/licenses/>.
+"""
+"""
+
+###############################################################################
+## Imports
+###############################################################################
+
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+###############################################################################
+## Functions
+###############################################################################
+
+# Default index
+def index(request):
+    return render_to_response('index.html',
+                              context_instance=RequestContext(request))
+
+
