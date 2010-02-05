1734719493cd1ce49a070e657821e5f8159637a4
Brandon Edens <brandon@cs.uri.edu>
2010-02-05 15:10:03 -0500

    More updates to the song views.

--- song/views.py
+++ song/views.py
@@ -10,6 +10,7 @@
 
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
+from django.http import HttpResponse
 from django.http import HttpResponseForbidden
 from django.shortcuts import get_object_or_404
 from django.views.generic.list_detail import object_list
@@ -54,12 +55,15 @@ def delete(request, object_id):
     """
     song = get_object_or_404(Song, pk=object_id)
     if request.user != song.artist.user:
-        # Song is not owned by this user. Do not allow them to delete this
-        # song.
+        # Song is not owned by this user. Do not allow delete.
         request.user.message_set.create(
             message="This song does not belong to you. You do not have permission to delete it."
             )
         return HttpResponseForbidden()
+    return delete_object(request,
+                         model=Song,
+                         object_id=song.id,
+                         post_delete_redirect=reverse('song_detail', args=[song.artist.id]))
 
 def list_by_letter(request, letter):
     """
@@ -70,9 +74,35 @@ def list_by_letter(request, letter):
                        template_object_name='song',
                        paginate_by=settings.SONGS_PER_PAGE,)
 
+def play(request, object_id):
+    """
+    Return the song's data appropriate for playing.
+    """
+    song = get_object_or_404(Song, pk=object_id)
+    if request.user != song.artist.user:
+        # Song is not owned by this user; do not allow play.
+        request.user.message_set.create(
+            message='This song does not belong to you. You cannot play it.'
+            )
+        return HttpResponseForbidden()
+    # Open the song file and return it as response.
+    response = HttpResponse(mimetype='audio/mpeg')
+    response.write(song.file.read())
+    return response
+
 @login_required
 def update(request, object_id):
     """
     """
-    pass
+    song = get_object_or_404(Song, pk=object_id)
+    if request.user != song.artist.user:
+        # Song is not owned by this user; do not allow update.
+        request.user.message_set.create(
+            message='This song does not belong to you. You cannot update it.'
+            )
+        return HttpResponseForbidden()
+    return update_object(request,
+                         form_class=SongForm,
+                         object_id=song.id,
+                         template_object_name='song')
 
