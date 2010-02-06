54428855444a002198a1bb870c8a1230d38524de
Brandon Edens <brandon@cs.uri.edu>
2010-02-06 12:54:32 -0500

    Add tests for the artist model.

--- artist/tests.py
+++ artist/tests.py
@@ -8,35 +8,64 @@
 ## Imports
 ###############################################################################
 
+from django.contrib.auth.models import User
 from django.test import TestCase
+
+from jukebox.artist.models import Artist
 
 
 ###############################################################################
 ## Classes
 ###############################################################################
 
-class SimpleTest(TestCase):
+class ArtistTest(TestCase):
 
     def setUp(self):
         """
         """
-        pass
+        self.user = User.objects.create(
+            username='jdoe',
+            first_name = 'John',
+            last_name = 'Doe',
+            email = 'jdoe@as220.org',
+            password = 'jojasdf23',
+            )
+        self.artist = Artist(user = self.user,
+                             name = 'The Edens Experience',
+                             description = 'This is a test artist.',
+                             email_address = 'brandon@as220.org',
+                             telephone_number = '401-529-0760',
+                             pay_to_the_order_of = 'Brandon Edens',
+                             address1 = '115 Empire St.',
+                             city = 'Providence',
+                             state = 'RI',
+                             zipcode = '02903'
+                             )
+        self.artist.save()
 
-    def test_basic_addition(self):
+    def test_name_handling_the(self):
         """
-        Tests that 1 + 1 always equals 2.
         """
-        self.failUnlessEqual(1 + 1, 2)
+        self.artist.name = 'tHe Edens Experience'
+        self.artist.save()
+        # Test that name is stored correctly.
+        self.assertEquals(self.artist.name, 'Edens Experience, tHe')
+        # Test that name is printed correctly.
+        self.assertEquals(self.artist.__unicode__(), 'tHe Edens Experience')
 
+    def test_name_handling_almost_the(self):
+        """
+        """
+        self.artist.name = 'Theo Edens Experience'
+        self.artist.save()
+        # Test that name is not modified.
+        self.assertEquals(self.artist.name, 'Theo Edens Experience')
+        # Test that not modified name is printed correctly.
+        self.assertEquals(self.artist.__unicode__(), 'Theo Edens Experience')
 
-###############################################################################
-## Statements
-###############################################################################
-
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
+    def test_absolute_url(self):
+        """
+        """
+        self.assertEquals(self.artist.get_absolute_url(),
+                          "/artist/detail/%d/" % self.artist.id)
 
