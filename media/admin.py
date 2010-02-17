221f5c43889440a3b9d220614948ebd562ae0af6
Brandon Edens <brandon@cs.uri.edu>
2010-02-17 16:16:35 -0500

    Add the SongAdmin to the administration for media.

--- media/admin.py
+++ media/admin.py
@@ -25,17 +25,15 @@
 
 from django.contrib import admin
 
-from jukebox.media.models import Artist, Photo, Song
-
-
-###############################################################################
-## Constants
-###############################################################################
+from jukebox.media.models import Artist, Album, Genre, Photo, Song
 
 
 ###############################################################################
 ## Classes
 ###############################################################################
+
+class SongAdmin(admin.ModelAdmin):
+    exclude = ('bitrate', 'variable_bitrate', 'duration', 'sample_frequency')
 
 
 ###############################################################################
@@ -43,6 +41,8 @@ from jukebox.media.models import Artist, Photo, Song
 ###############################################################################
 
 admin.site.register(Artist)
+admin.site.register(Album)
+admin.site.register(Genre)
 admin.site.register(Photo)
-admin.site.register(Song)
+admin.site.register(Song, SongAdmin)
 
