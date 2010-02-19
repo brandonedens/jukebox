b0e3afbea923cb921f47ce775dcff4ece4f4ad54
Brandon Edens <brandon@cs.uri.edu>
2010-02-19 12:20:24 -0500

    Update the review tablesorter code.

--- static_media/js/review/base.js
+++ static_media/js/review/base.js
@@ -1,11 +1,17 @@
 
-
 $(document).ready(function()
     {
-      $(".songs-sorted").tablesorter(
-        {
-          headers: {3: {sorter: false}, 4: {sorter: false} }
-        });
+      $('table.sorted').tablesorter(
+      {
+        headers: {
+          0: { sorter: 'anchor_text'},
+          1: { sorter: 'anchor_text'},
+          3: { sorter: false },
+          4: { sorter: false }
+        },
+        sortList: [[0,0], [1,0]],
+        widgets: ['zebra']
+      });
     }
 );
 
