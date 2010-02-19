b57b2b2b9d266d1683a61be367c02a49991b413a
Brandon Edens <brandon@cs.uri.edu>
2010-02-19 12:21:07 -0500

    Add the profile base javascript.

---
+++ static_media/js/profile/base.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function()
+    {
+      $("table.sorted").tablesorter(
+        {
+          headers: {
+            0: { sorter: 'anchor_text' },
+            1: { sorter: 'anchor_text' },
+            3: { sorter: false }
+          },
+          sortList: [[0,0], [1,0]],
+          widgets: ['zebra']
+        });
+    }
+);
+
