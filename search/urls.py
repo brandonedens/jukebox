6e32fe3e9c4c9f4bc0bc333389b814174fe6e518
Brandon Edens <brandon@cs.uri.edu>
2010-02-18 19:23:45 -0500

    Add the search application.

---
+++ search/urls.py
@@ -0,0 +1,49 @@
+# Brandon Edens
+# 2010-02-18
+# Copyright (C) 2010 Brandon Edens <brandon@as220.org>
+#
+# This file is part of jukebox.
+#
+# jukebox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# jukebox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with jukebox. If not, see <http://www.gnu.org/licenses/>.
+"""
+"""
+
+###############################################################################
+## Imports
+###############################################################################
+
+from django.conf.urls.defaults import *
+
+from jukebox.search.views import search
+
+
+###############################################################################
+## Constants
+###############################################################################
+
+urlpatterns = patterns('jukebox.search.views',
+    url(r'^$', search, name='search'),
+)
+
+
+###############################################################################
+## Classes
+###############################################################################
+
+
+###############################################################################
+## Functions
+###############################################################################
+
+
---
+++ search/views.py
@@ -0,0 +1,48 @@
+# Brandon Edens
+# 2010-01-09
+# Copyright (C) 2010 Brandon Edens <brandon@as220.org>
+"""
+"""
+
+###############################################################################
+## Imports
+###############################################################################
+
+from django.views.generic.simple import direct_to_template\
+
+from jukebox.music.models import Artist, Song, Genre
+from jukebox.search.forms import SearchForm
+
+
+###############################################################################
+## Functions
+###############################################################################
+
+def search(request):
+
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            search = form.cleaned_data['search']
+            terms = search.split(' ')
+            # Exact matches
+            artist_list = Artist.objects.filter(name__icontains=search)
+            song_list = Song.objects.filter(title__icontains=search)
+
+            # Partial matches
+            for term in terms:
+                artist_list = artist_list | Artist.objects.filter(name__icontains=term)
+                song_list = song_list | Song.objects.filter(title__icontains=term)
+
+            genre_list = Genre.objects.filter(name__icontains=search)
+            return direct_to_template(request, template='search/results.html',
+                                      extra_context={
+                                          'artist_list': artist_list,
+                                          'song_list': song_list,
+                                          'genre_list': genre_list,
+                                          'form': form,
+                                          },)
+    else:
+        form = SearchForm()
+    return direct_to_template(request, 'search/search_form.html', {'form': form,})
+
