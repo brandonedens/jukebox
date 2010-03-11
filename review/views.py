8c4538e31a6b54e5b3caadc97ceee824c14b8149
Brandon Edens <brandon@cs.uri.edu>
2010-03-11 13:32:42 -0500

    Update the accept/reject views for review.

--- review/views.py
+++ review/views.py
@@ -10,7 +10,9 @@
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import permission_required
-from django.views.generic.simple import direct_to_template
+from django.core.urlresolvers import reverse
+from django.shortcuts import get_object_or_404
+from django.views.generic.simple import direct_to_template, redirect_to
 
 from jukebox.music.models import Song, Photo
 
@@ -33,10 +35,18 @@ def index(request):
 @login_required
 @permission_required('music.song.can_review')
 def song_accept(request, song_id):
-    pass
+    song = get_object_or_404(Song, pk=song_id)
+    song.reviewed = True
+    song.save()
+    song.artist.user.message_set.create(message="Your song %s was approved." % song)
+
+    return redirect_to(request, reverse('review_songs'))
 
 @login_required
 @permission_required('music.song.can_review')
 def song_reject(request, song_id):
-    pass
+    song = get_object_or_404(Song, pk=song_id)
+    song.reviewed = False
+    song.save()
+    return redirect_to(request, reverse('review_songs'))
 
