f5290d24f4c36042c218951562b28dda7369aa97
Brandon Edens <brandon@cs.uri.edu>
2010-03-11 12:37:26 -0500

    Fix registration bugs.

--- registration/forms.py
+++ registration/forms.py
@@ -89,9 +89,9 @@ class RegistrationForm(forms.Form):
         """
         new_user = RegistrationProfile.objects.create_inactive_user(username=self.cleaned_data['username'],
                                                                     password=self.cleaned_data['password1'],
-                                                                    email=self.cleaned_data['email'],
                                                                     first_name=self.cleaned_data['first_name'],
                                                                     last_name=self.cleaned_data['last_name'],
+                                                                    email=self.cleaned_data['email'],
                                                                     profile_callback=profile_callback)
         return new_user
 
