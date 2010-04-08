ab2090f7bbac2a7cffe2ce54b225b4f97102a7ae
Brandon Edens <brandon@as220.org>
2010-04-08 15:29:43 -0400

    Further updates to the footer gui element.

--- jukebox/client/footer.py
+++ jukebox/client/footer.py
@@ -25,8 +25,9 @@
 
 from django.conf import settings
 import clutter
+import logging
 
-from playlist import is_now_playing, now_playing
+from playlist import is_now_playing, now_playing, has_next_song, next_song
 
 
 ###############################################################################
@@ -38,7 +39,7 @@ class Footer(clutter.Box):
     def __init__(self):
         super(Footer, self).__init__(clutter.BoxLayout())
 
-        self.set_color(clutter.Color(0, 0, 0, 240))
+        self.set_color(clutter.Color(0, 0, 0, 0))
 
         layout = self.get_layout_manager()
         layout.set_vertical(True)
@@ -46,60 +47,77 @@ class Footer(clutter.Box):
 
         self.set_width(settings.SCREEN_WIDTH)
 
-        self.now_playing = clutter.Box(clutter.BoxLayout())
-        tmplayout = self.now_playing.get_layout_manager()
+        self.now_playing = now_playing()
+
+        self.now_playing_box = clutter.Box(clutter.BoxLayout())
+        tmplayout = self.now_playing_box.get_layout_manager()
         tmplayout.set_vertical(False)
         tmplayout.set_spacing(20)
-        self.next_song = clutter.Box(clutter.BoxLayout())
-        tmplayout = self.next_song.get_layout_manager()
+        self.next_song_box = clutter.Box(clutter.BoxLayout())
+        tmplayout = self.next_song_box.get_layout_manager()
         tmplayout.set_vertical(False)
         tmplayout.set_spacing(20)
 
-        layout.pack(self.now_playing, True, False, False,
+        layout.pack(self.now_playing_box, True, False, False,
                     clutter.BOX_ALIGNMENT_CENTER,
                     clutter.BOX_ALIGNMENT_CENTER)
-        layout.pack(self.next_song, True, False, False,
+        layout.pack(self.next_song_box, True, False, False,
                     clutter.BOX_ALIGNMENT_CENTER,
                     clutter.BOX_ALIGNMENT_CENTER)
 
 
     def display(self):
-        if jukebox.is_now_playing():
-            song = jukebox.now_playing()
-            text = clutter.Text(settings.FOOTER_TEXT, 'Now playing:')
-            text.set_color(clutter.Color(200, 200, 200))
-            self.now_playing.add(text)
-            text = clutter.Text(settings.FOOTER_SONG_TEXT, song.title)
-            text.set_color(clutter.Color(240, 240, 40))
-            self.now_playing.add(text)
-            try:
-                print song.artist.name
-            except jukebox.music.models.DoesNotExist:
-                print 'got here'
-            text = clutter.Text(settings.FOOTER_ARTIST_TEXT,
-                                "by %s" % song.artist.name)
-            text.set_color(clutter.Color(240, 240, 40))
-            self.now_playing.add(text)
+        if is_now_playing() and self.now_playing != now_playing():
+            self.now_playing = now_playing()
+            self.set_color(clutter.Color(0, 0, 0, 250))
+            self.set_opacity(250)
 
-        if jukebox.has_next_song():
-            song = jukebox.next_song()
-            text = clutter.Text(settings.FOOTER_TEXT, 'Next song:')
+            # Remove existing text.
+            for actor in self.now_playing_box.get_children():
+                self.now_playing_box.remove(actor)
+
+            song = now_playing()
+            text = clutter.Text(settings.FOOTER_FONT, 'Now playing:')
             text.set_color(clutter.Color(200, 200, 200))
-            self.next_song.add(text)
-            text = clutter.Text(settings.FOOTER_SONG_TEXT, song.title)
+            self.now_playing_box.add(text)
+            text = clutter.Text(settings.FOOTER_SONG_FONT, song)
             text.set_color(clutter.Color(240, 240, 40))
-            self.next_song.add(text)
-            artist = None
-            text = clutter.Text(settings.FOOTER_ARTIST_TEXT,
-                                "by %s" % song.artist.name)
-            text.set_color(clutter.Color(240, 240, 40))
-            self.next_song.add(text)
+            self.now_playing_box.add(text)
+            try:
+                text = clutter.Text(settings.FOOTER_ARTIST_FONT,
+                                    "by %s" % song.artist)
+                text.set_color(clutter.Color(240, 240, 40))
+                self.now_playing_box.add(text)
+            except Exception:
+                pass
+
+            if has_next_song():
+                song = next_song()
+
+                # Remove existing text.
+                for actor in self.next_song_box.get_children():
+                    self.next_song_box.remove(actor)
+
+                text = clutter.Text(settings.FOOTER_FONT, 'Next song:')
+                text.set_color(clutter.Color(200, 200, 200))
+                self.next_song_box.add(text)
+                text = clutter.Text(settings.FOOTER_SONG_FONT, song)
+                text.set_color(clutter.Color(240, 240, 40))
+                self.next_song_box.add(text)
+                try:
+                    text = clutter.Text(settings.FOOTER_ARTIST_FONT,
+                                        "by %s" % song.artist)
+                    text.set_color(clutter.Color(240, 240, 40))
+                    self.next_song_box.add(text)
+                except Exception:
+                    pass
+
+            self.animation = self.animate(clutter.EASE_IN_CUBIC,
+                                          settings.FOOTER_FADE_RATE,
+                                          'opacity', 0)
 
     def update(self):
-        pass
-
-
-
+        self.display()
 
 
 ###############################################################################
