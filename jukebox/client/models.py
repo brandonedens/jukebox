f604aa8e643b06bf903c90f7035d58453a31c4f1
Brandon Edens <brandon@as220.org>
2010-04-09 15:34:49 -0400

    Add the client models file which will now hold the play, queue, and coin insert history.

---
+++ jukebox/client/models.py
@@ -0,0 +1,86 @@
+# Brandon Edens
+# 2010-04-09
+# Copyright (C) 2010 Brandon Edens <brandon@as220.org>
+#
+# This file is part of jukebox.
+#
+# jukebox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# jukebox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with jukebox. If not, see <http://www.gnu.org/licenses/>.
+"""
+"""
+
+###############################################################################
+## Imports
+###############################################################################
+
+from jukebox.music.models import Song
+
+
+###############################################################################
+## Classes
+###############################################################################
+
+class CoinInsert(models.Model):
+    """
+    The coin insert model.
+    """
+    added_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('added_on',)
+
+class QueuedPlay(models.Model):
+    """
+    The playlist queue.
+    """
+    song = models.ForeignKey(Song)
+
+    paid = models.BooleanField(default=False)
+    random = models.BooleanField(default=False)
+
+    added_on = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        ordering = ('added_on',)
+
+    def __unicode__(self):
+        return "%s - %s - %s" % (self.added_on, self.song.artist, self.song)
+
+    def __str__(self):
+        return self.__unicode__()
+
+class Play(models.Model):
+    """
+    Model that represents when a song was played.
+    """
+    song = models.ForeignKey(Song)
+    paid = models.BooleanField(default=False)
+    random = models.BooleanField(default=False)
+    played_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('played_on',)
+
+    def __unicode__(self):
+        text = ""
+        if self.paid:
+            text += 'PAID '
+        if self.random:
+            text += 'RANDOM '
+        text += "%s - %s - %s" % (self.played_on, self.song.artist, self.song)
+        return text
+
+    def __str__(self):
+        return self.__unicode__()
+
