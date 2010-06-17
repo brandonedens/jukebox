ead4f8b538af0d31b66d2cbe233a76ac1216d716
Brandon Edens <brandon@as220.org>
2010-06-17 18:24:00 -0400

    Add front page sorting of the play table.

--- jukebox/static_media/js/front.js
+++ jukebox/static_media/js/front.js
@@ -11,5 +11,17 @@ $(document).ready(
         $(".cover", this).stop().animate({top:'0px'},
                                          {queue:false,duration:300});
       });
+
+    $('table.sorted').tablesorter(
+    {
+      headers: {
+          0: { sorter: true },
+          1: { sorter: 'anchor_text' },
+          2: { sorter: 'anchor_text' },
+          3: { sorter: true }
+        },
+        sortList: [[0,0], [1,0]],
+        widgets: ['zebra']
+    });
 });
 
